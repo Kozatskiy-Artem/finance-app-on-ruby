02ba66a409db2c15e6fc881b7848aec53df908d8
Artem Kozatskyi <artemfury@gmail.com>
2023-03-04 01:13:20 +0200

    Generate conrollers Main and Reports, development of application pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
+  root "main#index"
+
+  get 'main/index'
+  get 'reports/index'
+  get 'reports/report_by_category'
+  get 'reports/report_by_dates'
+
   resources :operations
   resources :categories
+  resources :reports
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
